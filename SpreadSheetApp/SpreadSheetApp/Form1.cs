a8198d122ac35070f78a03d39d871dd422f9be1a
nkarl <21993921+nkarl@users.noreply.github.com>
2022-06-04 13:59:23 -0700

    Setup; Initialized the app display shell (dataGridView component)

--- SpreadSheetApp/SpreadSheetApp/Form1.cs
+++ SpreadSheetApp/SpreadSheetApp/Form1.cs
@@ -1,10 +1,48 @@
+// <copyright file="Form1.cs" company="Charles Nguyen -- 011606177">
+// Copyright (c) Charles Nguyen -- 011606177. All rights reserved.
+// </copyright>
+
 namespace SpreadSheetApp
 {
+    using SpreadSheetApp.Form1_Properties;
+
+    /// <summary>
+    /// Controls the display of the data grid cells.
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Initialize_GridDimensions();
+        }
+
+        /// <summary>
+        /// Initializes the dimensions of the data grid.
+        /// </summary>
+        private void Initialize_GridDimensions()
+        {
+            foreach (var c in GridDimensions.ColumnLabels)
+            {
+                var col = new DataGridViewTextBoxColumn();
+                col.HeaderText = c.ToString();
+                this.dataGridView1.Columns.Add(col);
+            }
+
+            var maxRows = Enumerable.Range(0, GridDimensions.MaxColumns);
+            foreach (var i in maxRows)
+            {
+                var row = new DataGridViewRow();
+                row.HeaderCell.Value = (i + 1).ToString();
+                this.dataGridView1.Rows.Add(row);
+            }
         }
     }
 }
---
+++ SpreadSheetApp/SpreadSheetApp/Form1_Properties/GridDimensions.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="GridDimensions.cs" company="Charles Nguyen -- 011606177">
+// Copyright (c) Charles Nguyen -- 011606177. All rights reserved.
+// </copyright>
+
+namespace SpreadSheetApp.Form1_Properties
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Controls the dimensions of the data grid, i.e. how many rows and columns to show.
+    /// </summary>
+    internal static class GridDimensions
+    {
+        /// <summary>
+        /// The array of characters used for labeling columns.
+        /// </summary>
+        public static readonly char[] ColumnLabels = (
+            from c in Enumerable.Range('A', 'Z' - 'A' + 1)
+            select (char)c).ToArray();
+
+        /// <summary>
+        /// The maximum number of rows allowed.
+        /// </summary>
+        public static readonly int MaxRows = 50;
+
+        /// <summary>
+        /// The maximum number of columns allowed.
+        /// </summary>
+        public static readonly int MaxColumns = GridDimensions.ColumnLabels.Length;
+    }
+}
