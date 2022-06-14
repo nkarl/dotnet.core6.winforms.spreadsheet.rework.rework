7df5a4b05b902a7011c9c62862cb92f2a099ab34
nkarl <21993921+nkarl@users.noreply.github.com>
2022-06-13 23:38:48 -0700

    Added some code for class libray SpreadSheetEngine.

---
+++ SpreadSheetApp/SpreadSheetEngine/CellBase.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="CellBase.cs" company="Charles Nguyen -- 011606177">
+// Copyright (c) Charles Nguyen -- 011606177. All rights reserved.
+// </copyright>
+
+namespace SpreadSheetEngine
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The abstract base classs for a single cell.
+    /// </summary>
+    public abstract class CellBase : INotifyPropertyChanged
+    {
+        private string text = string.Empty;
+        private string value = string.Empty;
+
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets the text of the cell.
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+
+            set
+            {
+                if (value == this.text)
+                {
+                    return;
+                }
+
+                this.text = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets the value of the cell.
+        /// </summary>
+        public string Value
+        {
+            get { return this.value; }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
