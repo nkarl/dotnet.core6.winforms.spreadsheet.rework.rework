a456d8322635f2c1d7367b2ada26447d2de643cf
nkarl <21993921+nkarl@users.noreply.github.com>
2022-06-18 23:35:57 -0700

    Set up this branch for HW5: building an expression tree.

---
+++ SpreadSheetEngine/ExpressionTree.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ExpressionTree.cs" company="Charles Nguyen -- 011606177">
+// Copyright (c) Charles Nguyen -- 011606177. All rights reserved.
+// </copyright>
+
+namespace SpreadSheetEngine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SpreadSheetEngine.Interface;
+
+    /// <summary>
+    /// The data structure for arithmetic expressions.
+    /// </summary>
+    internal class ExpressionTree : IExpressionParser
+    {
+        /*
+            TODO:
+                - Implements the class ExpressionTree from IExpressionParser.
+         */
+    }
+}
---
+++ SpreadSheetEngine/Interface/IExpressionParser.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ExpressionParser.cs" company="Charles Nguyen -- 011606177">
+// Copyright (c) Charles Nguyen -- 011606177. All rights reserved.
+// </copyright>
+
+namespace SpreadSheetEngine.Interface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The parser interface that helps build a tree from an arithmetic expression.
+    /// </summary>
+    internal interface IExpressionParser
+    {
+        /*
+            TODO:
+                - Read guidelines on how to write an interface in C#.
+                - Decide and design the interface methods/functions.
+         */
+    }
+}
