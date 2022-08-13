975b0fccc956e958bb5bff017467cd77a51782da
nkarl <21993921+nkarl@users.noreply.github.com>
2022-08-12 17:15:50 -0700

    Added more test case for the parser and postfix maker.

--- ExpressionTreeTests/ExpressionParserTests.cs
+++ ExpressionTreeTests/ExpressionParserTests.cs
@@ -16,6 +16,8 @@ public class ExpressionParserTests
     [TestCase("A1+B2+C3", new [] {"VarNode", "OpNodeAdd", "VarNode", "OpNodeAdd", "VarNode"})]
     [TestCase("A1+22+C3", new [] {"VarNode", "OpNodeAdd", "ConstNode", "OpNodeAdd", "VarNode"})]
     [TestCase("11+22+33", new [] {"ConstNode", "OpNodeAdd", "ConstNode", "OpNodeAdd", "ConstNode"})]
+    [TestCase("11-22*33", new [] {"ConstNode", "OpNodeSub", "ConstNode", "OpNodeMul", "ConstNode"})]
+    [TestCase("A1+B2-C3*D4/E5", new [] {"VarNode", "OpNodeAdd", "VarNode", "OpNodeSub", "VarNode", "OpNodeMul", "VarNode", "OpNodeDiv", "VarNode"})]
     public void ParseNodeFromStringTest(string input, string[] expected)
     {
         var blocks = ExpressionParser.FromStrToBlocks(input);
@@ -28,6 +30,7 @@ public class ExpressionParserTests
     }
 
     [TestCase("A1+B2+C3", new [] {"VarNode", "VarNode", "OpNodeAdd", "VarNode", "OpNodeAdd"})]
+    [TestCase("A1+22+C3", new [] {"VarNode", "ConstNode", "OpNodeAdd", "VarNode", "OpNodeAdd"})]
     public void ConvertNodesToPostfixTest(string input, string[] expected)
     {
         var blocks = ExpressionParser.FromStrToBlocks(input);
