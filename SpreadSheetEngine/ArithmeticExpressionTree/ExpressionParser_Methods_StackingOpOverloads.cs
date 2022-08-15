e4ba79e9120f973b10216d4ffb8cbdfb1ae6c496
nkarl <21993921+nkarl@users.noreply.github.com>
2022-08-14 18:22:40 -0700

    format.

--- SpreadSheetEngine/ArithmeticExpressionTree/ExpressionParser_Methods_StackingOpOverloads.cs
+++ SpreadSheetEngine/ArithmeticExpressionTree/ExpressionParser_Methods_StackingOpOverloads.cs
@@ -17,98 +17,98 @@ namespace SpreadSheetEngine.ArithmeticExpressionTree
     {
         private static void StackingAdd(OpNodeAdd incomingOp, Stack<Node> stack, List<Node> postfix)
         {
-                    if (stack.Count > 0)
+            if (stack.Count > 0)
+            {
+                if (incomingOp.Precedence == ((OpNodeAdd)stack.Peek()).Precedence)
+                {
+                    if (incomingOp.Associativity == OpAssociativity.Leftward)
                     {
-                        if (incomingOp.Precedence == ((OpNodeAdd)stack.Peek()).Precedence)
-                        {
-                            if (incomingOp.Associativity == OpAssociativity.Leftward)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
-                        else if (incomingOp.Precedence < ((OpNodeAdd)stack.Peek()).Precedence)
-                        {
-                            // pop stack and add to postfix, and then continue the same test on the new top.
-                            for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeAdd)stack.Peek()).Precedence;)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
+                        postfix.Add(stack.Pop());
                     }
+                }
+                else if (incomingOp.Precedence < ((OpNodeAdd)stack.Peek()).Precedence)
+                {
+                    // pop stack and add to postfix, and then continue the same test on the new top.
+                    for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeAdd)stack.Peek()).Precedence;)
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                }
+            }
 
-                    stack.Push(incomingOp);
+            stack.Push(incomingOp);
         }
 
         private static void StackingSub(OpNodeSub incomingOp, Stack<Node> stack, List<Node> postfix)
         {
-                    if (stack.Count > 0)
+            if (stack.Count > 0)
+            {
+                if (incomingOp.Precedence == ((OpNodeSub)stack.Peek()).Precedence)
+                {
+                    if (incomingOp.Associativity == OpAssociativity.Leftward)
                     {
-                        if (incomingOp.Precedence == ((OpNodeSub)stack.Peek()).Precedence)
-                        {
-                            if (incomingOp.Associativity == OpAssociativity.Leftward)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
-                        else if (incomingOp.Precedence < ((OpNodeSub)stack.Peek()).Precedence)
-                        {
-                            // pop stack and add to postfix, and then continue the same test on the new top.
-                            for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeSub)stack.Peek()).Precedence;)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
+                        postfix.Add(stack.Pop());
                     }
+                }
+                else if (incomingOp.Precedence < ((OpNodeSub)stack.Peek()).Precedence)
+                {
+                    // pop stack and add to postfix, and then continue the same test on the new top.
+                    for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeSub)stack.Peek()).Precedence;)
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                }
+            }
 
-                    stack.Push(incomingOp);
+            stack.Push(incomingOp);
         }
 
         private static void StackingMul(OpNodeMul incomingOp, Stack<Node> stack, List<Node> postfix)
         {
-                    if (stack.Count > 0)
+            if (stack.Count > 0)
+            {
+                if (incomingOp.Precedence == ((OpNodeMul)stack.Peek()).Precedence)
+                {
+                    if (incomingOp.Associativity == OpAssociativity.Leftward)
                     {
-                        if (incomingOp.Precedence == ((OpNodeMul)stack.Peek()).Precedence)
-                        {
-                            if (incomingOp.Associativity == OpAssociativity.Leftward)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
-                        else if (incomingOp.Precedence < ((OpNodeMul)stack.Peek()).Precedence)
-                        {
-                            // pop stack and add to postfix, and then continue the same test on the new top.
-                            for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeMul)stack.Peek()).Precedence;)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
+                        postfix.Add(stack.Pop());
                     }
+                }
+                else if (incomingOp.Precedence < ((OpNodeMul)stack.Peek()).Precedence)
+                {
+                    // pop stack and add to postfix, and then continue the same test on the new top.
+                    for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeMul)stack.Peek()).Precedence;)
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                }
+            }
 
-                    stack.Push(incomingOp);
+            stack.Push(incomingOp);
         }
 
         private static void StackingDiv(OpNodeDiv incomingOp, Stack<Node> stack, List<Node> postfix)
         {
-                    if (stack.Count > 0)
+            if (stack.Count > 0)
+            {
+                if (incomingOp.Precedence == ((OpNodeDiv)stack.Peek()).Precedence)
+                {
+                    if (incomingOp.Associativity == OpAssociativity.Leftward)
                     {
-                        if (incomingOp.Precedence == ((OpNodeDiv)stack.Peek()).Precedence)
-                        {
-                            if (incomingOp.Associativity == OpAssociativity.Leftward)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
-                        else if (incomingOp.Precedence < ((OpNodeMul)stack.Peek()).Precedence)
-                        {
-                            // pop stack and add to postfix, and then continue the same test on the new top.
-                            for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeDiv)stack.Peek()).Precedence;)
-                            {
-                                postfix.Add(stack.Pop());
-                            }
-                        }
+                        postfix.Add(stack.Pop());
                     }
+                }
+                else if (incomingOp.Precedence < ((OpNodeMul)stack.Peek()).Precedence)
+                {
+                    // pop stack and add to postfix, and then continue the same test on the new top.
+                    for (; stack.Count > 0 && incomingOp.Precedence < ((OpNodeDiv)stack.Peek()).Precedence;)
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                }
+            }
 
-                    stack.Push(incomingOp);
+            stack.Push(incomingOp);
         }
     }
 }
