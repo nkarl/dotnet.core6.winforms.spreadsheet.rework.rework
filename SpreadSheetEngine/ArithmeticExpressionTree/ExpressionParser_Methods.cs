090680485eaa6a7b2839ce5f2d07621cf15d62ae
nkarl <21993921+nkarl@users.noreply.github.com>
2022-08-15 06:49:52 -0700

    Removed the WIP brace method on this branch.

--- SpreadSheetEngine/ArithmeticExpressionTree/ExpressionParser_Methods.cs
+++ SpreadSheetEngine/ArithmeticExpressionTree/ExpressionParser_Methods.cs
@@ -108,65 +108,6 @@ namespace SpreadSheetEngine.ArithmeticExpressionTree
         }
 
         /// <summary>
-        ///     Parse expression with braces accounted for.
-        /// </summary>
-        /// <param name="infix">the original expression.</param>
-        internal static void ParseInfixWithBraces(string infix)
-        {
-            // TODO: IMPLEMENT THE DECOMPOSING LOGIC FOR PARENTHESES.
-            // var expression = "(A1+B2)+C3";
-            var braces = "{[()]}"; // the braces should probably be put into a dict as key-value pair.
-
-            Dictionary<char, char> localBraceDict = new()
-            {
-                { '{', '}' },
-                { '[', ']' },
-                { '(', ')' },
-            };
-
-            Dictionary<char, Func<char>> localOperatorDict = new()
-            {
-                { '+', () => '+' },
-                { '-', () => '-' },
-                { '*', () => '*' },
-                { '/', () => '/' },
-            };
-
-            var operatorList = (
-                from op in localOperatorDict
-                select op.Key).ToArray();
-
-            // var operatorList = opList.ToArray();
-
-            // first, parse only operators from the expression.
-            var operators = (
-                from c in infix
-                where operatorList.Contains(c)
-                select string.Empty + c).ToArray();
-
-            // then, parse only operands by splitting it by operators.
-            var operands = infix.Split(operatorList);
-
-            foreach (var block in operands)
-            {
-                if (block[0] is var openB && braces.Contains(openB))
-                {
-                    Console.WriteLine(openB);
-                    Console.WriteLine(block[1..]);
-                }
-                else if (block[^1] is var closeB && braces.Contains(closeB))
-                {
-                    Console.WriteLine(block[..^1]);
-                    Console.WriteLine(closeB);
-                }
-                else
-                {
-                    Console.WriteLine(block);
-                }
-            }
-        }
-
-        /// <summary>
         ///     Converts block expression into node expression.
         /// </summary>
         /// <param name="blocks">expression as blocks of string.</param>
