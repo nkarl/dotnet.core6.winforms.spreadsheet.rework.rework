ac2b140db9a289e9b463bff9c1f7d471bfed5158
nkarl <21993921+nkarl@users.noreply.github.com>
2022-08-16 10:53:54 -0700

    Added a copy method for the postfix maker.

--- SpreadSheetEngine/ArithmeticExpressionTree/ExpressionParser_Methods_PostFix.cs
+++ SpreadSheetEngine/ArithmeticExpressionTree/ExpressionParser_Methods_PostFix.cs
@@ -30,6 +30,70 @@ namespace SpreadSheetEngine.ArithmeticExpressionTree
         /// </summary>
         /// <param name="infix">the infix as list of nodes.</param>
         /// <returns>new postfix as list of nodes.</returns>
+        public static IEnumerable<Node> MakePostfixWithBraces(IEnumerable<Node> infix)
+        {
+            var stack = new Stack<Node>();
+            var postfix = new List<Node>();
+            var leftbraces = "{[(";
+            var rightbraces = ")]}";
+            var braces = new Dictionary<char, char>()
+            {
+                { ')', '(' },
+                { ']', '[' },
+                { '}', '{' },
+            };
+
+            foreach (Node node in infix)
+            {
+                if (node is OpNode incoming)
+                {
+                    if (stack.Count > 0)
+                    {
+                        if (incoming.Precedence == ((OpNode)stack.Peek()).Precedence)
+                        {
+                            if (incoming.Associativity == OpAssociativity.Leftward)
+                            {
+                                postfix.Add(stack.Pop());
+                            }
+                        }
+                        else if (incoming.Precedence < ((OpNode)stack.Peek()).Precedence)
+                        {
+                            // pop stack and add to postfix, and then continue the same test on the new top.
+                            for (; stack.Count > 0 && incoming.Precedence < ((OpNode)stack.Peek()).Precedence;)
+                            {
+                                postfix.Add(stack.Pop());
+                            }
+                        }
+                    }
+
+                    stack.Push(incoming);
+                }
+                else
+                {
+                    /*
+                     * TODO: implements the logic for pushing and popping braces from stack.
+                     *  Description,
+                     *      1. whenever the symbol is any left brace, push it to the stack.
+                     *      2. whenever the symbol is any right brace, pop the stack until
+                     *          found its matching left brace. Then discard both.
+                     */
+                    postfix.Add(node);
+                }
+            }
+
+            for (; stack.Count > 0;)
+            {
+                postfix.Add(stack.Pop());
+            }
+
+            return postfix;
+        }
+
+        /// <summary>
+        ///     Make a postfix from an infix as list of nodes.
+        /// </summary>
+        /// <param name="infix">the infix as list of nodes.</param>
+        /// <returns>new postfix as list of nodes.</returns>
         public static IEnumerable<Node> MakePostfix(IEnumerable<Node> infix)
         {
             var stack = new Stack<Node>();
